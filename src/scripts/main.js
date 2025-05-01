ea3577f9fd07b3425810c9f39f27440d23637eb6
TaniaFedortsi <tania.fdrts@gmail.com>
2025-05-01 10:45:49 +0300

    fixed message problem

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,12 +4,17 @@ import Game from '../modules/Game.class';
 
 const game = new Game();
 
-game.start();
-
 const fieldElement = document.querySelector('.game-field tbody');
 const scoreElement = document.querySelector('.game-score');
-const statusElement = document.querySelector('.message-container .message');
+const messageElements = document.querySelectorAll(
+  '.message-container .message',
+);
+const messageLose = document.querySelector('.message-lose');
+const messageWin = document.querySelector('.message-win');
+const messageStart = document.querySelector('.message-start');
 const buttonStart = document.querySelector('.button.start');
+
+render();
 
 function render() {
   fieldElement.innerHTML = '';
@@ -31,10 +36,8 @@ function render() {
         cell.textContent = value;
         cell.classList.add(`field-cell--${value}`);
       }
-
       tr.appendChild(cell);
     }
-
     fieldElement.appendChild(tr);
   }
 
@@ -42,24 +45,22 @@ function render() {
 
   const stats = game.getStatus();
 
+  messageElements.forEach((el) => el.classList.add('hidden'));
+
   if (stats === 'win') {
-    statusElement.textContent = 'Winner! Congrats! You did it!';
-    statusElement.classList.remove('hidden');
+    messageWin.classList.remove('hidden');
+  } else if (stats === 'lose') {
+    messageLose.classList.remove('hidden');
   } else if (stats === 'idle') {
-    statusElement.textContent = 'Press "Start" to begin game. Good luck!';
-    statusElement.classList.remove('hidden');
-  } else {
-    statusElement.classList.add('hidden');
+    messageStart.classList.remove('hidden');
+    buttonStart.textContent = 'Start';
+    buttonStart.classList.add('start');
+    buttonStart.classList.remove('restart');
+  } else if (stats === 'playing') {
+    buttonStart.textContent = 'Restart';
+    buttonStart.classList.remove('start');
+    buttonStart.classList.add('restart');
   }
-
-  if (stats === 'lose') {
-    statusElement.textContent = 'You lose! Restart the game?';
-    statusElement.classList.remove('hidden');
-  }
-
-  buttonStart.textContent = 'Restart';
-  buttonStart.classList.remove('start');
-  buttonStart.classList.add('restart');
 }
 
 document.addEventListener('keydown', (ev) => {
@@ -85,6 +86,9 @@ document.addEventListener('keydown', (ev) => {
 });
 
 buttonStart.addEventListener('click', () => {
-  game.restart();
+  game.start();
   render();
+  buttonStart.textContent = 'Restart';
+  buttonStart.classList.remove('start');
+  buttonStart.classList.add('restart');
 });
